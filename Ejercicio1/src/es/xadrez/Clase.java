6fada607e1a1ab942e644fe79da7dc1737f4c299
Juan José Hidalgo Neira <xxhidalgoneira@gmail.com>
2021-12-11 18:04:56 +0100

    Ejercicio 1

---
+++ Ejercicio1/src/es/xadrez/Clase.java
@@ -0,0 +1,44 @@
+package es.xadrez;
+
+public class Clase {
+    public static void main(String[] args) {
+
+        byte variable1 = 4;
+        System.out.println(variable1);
+
+        short variable2 = 10;
+        System.out.println(variable2);
+
+        int variable3 = 300;
+        System.out.println(variable3);
+
+        long variable4 = 1000;
+        System.out.println(variable4);
+
+
+        // 1.2 decimales
+        float variable5 = 5.5f;
+        System.out.println(variable5);
+
+        double variable6 = 10.5d;
+        System.out.println(variable6);
+
+
+
+        // 2. booleanos
+        boolean variable7 = true;
+        System.out.println(variable7);
+
+        boolean variable8 = false;
+        System.out.println(variable8);
+
+        // 3. texto
+        char variable9 ='a';
+        System.out.println(variable9);
+
+        String variable10 = "Lorem ipsum dolor sit amen....";
+        System.out.println(variable10);
+
+
+    }
+}
